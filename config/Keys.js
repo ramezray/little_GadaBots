fb171fe57b5f7290501959820b4fa9067065f89c
unknown <ray_ramez@yahoo.com>
2019-04-08 18:09:10 -0700

    added M3 bucket

--- config/Keys.js
+++ config/Keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: process.env.MONGODB_URI || "mongodb+srv://ramezray:r2005moner@cluster0-kxkcm.mongodb.net/gadabots",
-    jwtSecret: process.env.JWT_SECRET || "s00persekrit"
-}
+  mongoURI:
+    process.env.MONGODB_URI ||
+    "mongodb+srv://ramezray:r2005moner@cluster0-kxkcm.mongodb.net/gadabots",
+  jwtSecret: process.env.JWT_SECRET || "s00persekrit"
+};
